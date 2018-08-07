88a5c76b34e17b9e07192262a721d3d6c18b955b
hosamhany <7ossHany@gmail.com>
2018-08-07 23:10:32 +0200

    getting the data of every user through ACCOUNTFOLLOWERS, trying to use their username to get the rest of the data

--- cliTool.js
+++ cliTool.js
@@ -3,30 +3,23 @@ const instagramApi = require ('./instagramPrivateApi/instagramApi')
 const prompt = require('prompt');
 const schema = require ('./utils/validations/promptValidations')
 
-
 program
     .version('0.0.1')
     .description('instagram cli tool');
+//this updates the session of the user
+program
+    .command('getFollowers <accountName>')
+    .alias('gf')
+    .description('gets the followers of the account input')
+    .action((accountName) => {
+            prompt.start();
+            console.log("Please login..")
+            
+            prompt.get(schema.schema, function (err, result) {
+                console.log("Fetching",accountName+"'s followers");
+                instagramApi.createSession(result.username, result.password, accountName);
+            })
 
-// function login(){
-    program
-        .command('help')
-        .alias('a')
-        .description('help')
-        .action(() => {
-                prompt.start();
-                console.log("Please login..")
-                prompt.get(schema.schema, function (err, result) {
-                    if(err){
-                        console.log('\x1b[31m', "An error has occured, please try again later.." )
-                    }
-                    instagramApi.createSession(result.username, result.password)
-                });           
-        });
-// }
-
-function help () {
-    console.log("help")
-}
+    });
 
 program.parse(process.argv);
--- instagramPrivateApi/instagramApi.js
+++ instagramPrivateApi/instagramApi.js
@@ -1,17 +1,53 @@
 var Client = require('instagram-private-api').V1;
 var device = new Client.Device('testDevice');
+var Promise = require('bluebird');
+var _ = require('lodash');
+var fs = require('fs');
+
 // var __dirname = "~"
 var storage = new Client.CookieFileStorage(__dirname + '/../utils/cookies/session.json');
 
-function createSession(username, password){
+function createSession(username, password, accountName){
     Client.Session.create(device, storage, username, password)
     .then((session) => {
-        console.log(session);
-        // console.log(session);
-
+        return [session, Client.Account.searchForUser(session, accountName)]
     })
+    .spread(function(session, account) {
+        // console.log(account)
+    let feed = new Client.Feed.AccountFollowers(session, account.id, parseInt(account.followerCount))
+    feed.all().then((accounts)=>{
+        let followers = [];
+        let followersUserNames = [];
+        for(let item of accounts){
+            followersUserNames.push(item._params.username);
+        }
+        for(let item of followersUserNames){
+        Client.Account.searchForUser(session, item).then((result) =>{
+            for (let item of result._params){
+                followers.push(item);
+            }
+        })
+
+    }
+        // console.log(followers)
+        // _.map(followersNew, followers => _.pick(followers, 'id','_params.username', '_params.fullName', '_params.profilePicUrl'))
+        // console.log(followersNew.length)
+        // console.log(followers);
+        // writeIntoFile(followers);
+        })
+    })
+    // .catch(()=>{
+    //     console.log("\x1b[31m", "Connection Error, please try again later.")
+    // });
 }
 
+function writeIntoFile(followers){
+    fs.writeFile(__dirname + "/../utils/followers.csv", followers, { flag: 'w' }, function (err) {
+        if (err) throw err;
+        console.log("It's saved!");
+    });
+}
 module.exports = {
     createSession
 }
+//node --max-old-space-size=4096 yourFile.js
